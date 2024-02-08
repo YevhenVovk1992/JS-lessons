6c29195756738044b361617b127594c41cef2d00
Yevhen <104986485+YevhenVovk1992@users.noreply.github.com>
2024-02-08 13:39:40 +0200

    Working with arrays
    Working with arrays;

---
+++ task7.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/* Напишите функцию reverse, которая принимает в себя строку и возвращает эту строку в обратном порядке.
+
+Пример:
+
+const someString = 'This is some strange string';
+reverse(someString) => 'gnirts egnarts emos si sihT'
+
+Функцию можно применить к любой строке. Если в функцию приходит не строка - вернуть сообщение "Ошибка!" */
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof(str) !== 'string') {
+        return "Ошибка!";
+    }
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+}
+
+console.log(reverse(someString))
+
+/* Представьте такую реальную ситуацию. У вас есть банкомат, который выдает деньги из двух разных банков в разных валютах.
+ Один банк основной с базовыми валютами, второй дополнительный с прочими валютами:
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+Вам нужно создать главную функцию банкомата availableCurr, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу 
+(сейчас представим, что они не могут повторяться), второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. 
+Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'. Функция возвращает строку в нужном виде. */
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    let str = '';
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+    arr.forEach(function(curr, i) {
+        if (curr !== missingCurr) {
+            str += `${curr}\n`;
+        }
+    });
+    return str;
+}
+
+
