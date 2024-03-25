bf2f6c76a53f6e11363ded7d6049ba108ee6d290
Yevhen <104986485+YevhenVovk1992@users.noreply.github.com>
2024-03-25 15:25:06 +0200

    Create tabs on the page
    attach the script and myStyle;
    added a function for hiding tab elements;
    added a function to show elements that are needed when you click at this tab;
    added event listener for tab parent;

---
+++ tabs/js/script.js
@@ -0,0 +1,40 @@
+'use strict'; 
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const tabsContent = document.querySelectorAll('.tabcontent'),
+        tabs = document.querySelectorAll('.tabheader__item'),
+        tabsParent = document.querySelector('.tabheader__items');
+ 
+   
+    function hideTab() {
+        tabsContent.forEach((item) => {
+            item.classList.add('hideTab');            
+        });
+        tabs.forEach((tab) => {
+            tab.classList.remove('tabheader__item_active');
+        });
+
+    }
+
+    function showTab(i = 0) {
+       tabsContent[i].classList.remove('hideTab');
+       tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTab();
+    showTab();
+    
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (item == target) {
+                    hideTab();
+                    showTab(i);
+                }
+            });
+        }
+    })
+});
